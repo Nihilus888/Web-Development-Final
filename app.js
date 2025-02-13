7093731c061af349c10530ab7297198c078af03f
NAME SURNAME <austonmartin@gmail.com>
2025-02-13 20:16:35 +0800

    added cors, API and display of data on frontend

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
+const { response } = require('express');
 const express = require('express');
 const app = express();
 const fs = require('fs');
+const cors = require('cors');
+
+// Enable CORs for all routes
+app.use(cors());
 
 // Serve static files from the "public" directory
 app.use(express.static('public'));
@@ -18,8 +23,21 @@ app.get('/home', (req, res) => {
 
 // Route to history page
 app.get('/history', (req, res) => {
-    res.render('history');
-})
+
+    fetch('http://localhost:3000/api/data')
+        .then(response => {
+            if(!response.ok) {
+                throw new Error("Unable to make API call");
+            }
+            return response.json();
+        })
+        .then(timelineData => {
+            res.render('history', { timelineData });
+        })
+        .catch(error => {
+            console.error("Error: ". error);
+        });
+});
 
 // Route to images page
 app.get('/images', (req, res) => {
@@ -36,7 +54,7 @@ app.get('/api/data', (req, res) => {
     const filePath = "./data/data.json";
 
     fs.readFile(filePath, 'utf-8', (err, data) => {
-        if(err) {
+        if (err) {
             return res.status(500).json("Issue with pulling the local json data file");
         }
 
@@ -45,10 +63,9 @@ app.get('/api/data', (req, res) => {
             res.json(jsonData);
         } catch (err) {
             res.status(500).json("Error parsing the json data");
-        };
+        }
     });
-})
-
+});
 // Start the server
 app.listen(3000, () => {
   console.log('Server is running on http://localhost:3000/home');
